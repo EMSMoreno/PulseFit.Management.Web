baba1dfcf7b5b47f873e95632730fd87ef747715
EMSMoreno <emsmoreno97@gmail.pt>
2024-10-02 10:56:09 +0100

    Account Views & Errors

---
+++ Controllers/ErrorsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PulseFit.Management.Web.Models;
+using System.Diagnostics;
+ 
+namespace PulseFit.Management.Web.Controllers
+{
+    public class ErrorsController : Controller
+    {
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        //Esta action vai ser executada quando entrar por este caminho error/404
+        [Route("error/404")]
+        //Este action é só para mostrar a página do Error404
+        //Depois de elaborado este metodo temos que criar a respectiva View para o Error404 para isso
+        //clicamos com o botao direito sobre Error404() e fazemos Add View - Razor View - Add
+        public IActionResult Error404()
+        {
+            return View();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -78,10 +78,13 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
+    app.UseExceptionHandler("/Errors/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+//Quando nao encontrar a página vai à procura de um error
+app.UseStatusCodePagesWithReExecute("/error/{0}");
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
